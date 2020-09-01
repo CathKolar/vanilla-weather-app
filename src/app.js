f6a20784420beeb20404f38144fb580a954b20d6
CathKolar <68386285+CathKolar@users.noreply.github.com>
2020-09-01 18:43:58 +0200

    showing current temp and weather conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  document.querySelector("#high-temp-today").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#low-temp-today").innerHTML = Math.round(
+  );
+  document.querySelector(
+    "#city"
+  ).innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  document.querySelector(".current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+}
+let apiKey = "6feaf6a8d604af91166c8484867322e7";
+let city = "Duluth";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+axios.get(apiUrl).then(displayTemperature);
