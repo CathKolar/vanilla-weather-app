b7f017e46f220c59ad98301d81c2151ec0a86e17
CathKolar <68386285+CathKolar@users.noreply.github.com>
2020-09-01 19:20:30 +0200

    update the time

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} <br/> ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   document.querySelector("#high-temp-today").innerHTML = Math.round(
     response.data.main.temp_max
   );
   document.querySelector("#low-temp-today").innerHTML = Math.round(
+    response.data.main.temp_min
   );
   document.querySelector(
     "#city"
@@ -15,6 +38,10 @@ function displayTemperature(response) {
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 let apiKey = "6feaf6a8d604af91166c8484867322e7";
 let city = "Duluth";
